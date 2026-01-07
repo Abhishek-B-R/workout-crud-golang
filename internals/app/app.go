4da8a3c738540872e4a28ce1a7de1382cef840de
Abhishek B R <byteblaster1468@gmail.com>
2026-01-08 00:09:23 +0530

    db migrations running successfully

--- internals/app/app.go
+++ internals/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/Abhishek-B-R/workout-crud/internals/api"
 	"github.com/Abhishek-B-R/workout-crud/internals/store"
+	"github.com/Abhishek-B-R/workout-crud/migrations"
 )
 
 type Application struct{
@@ -21,6 +22,11 @@ func NewApplication() (*Application, error){
 	pgDb, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDb, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
